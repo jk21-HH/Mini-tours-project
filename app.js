0aa40dd698636d118f7e0b56762c42370d3e8da7
jk21-HH <jeanya12@gmail.com>
2023-09-20 14:48:57 +0300

    prevent injections and xss

--- app.js
+++ app.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 
 const AppError = require('./utils/appError');
 
@@ -11,10 +15,38 @@ const app = express();
 
 // Midellwares
 
+// Set security http headers
+
+app.use(helmet());
+
+// Limit requsts from same API
+
+const limiter = rateLimit({
+  max: 15,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requsets from this IP, please try in an hour',
+});
+
+app.use('/api', limiter);
+
 app.use(morgan('dev'));
-app.use(express.json());
+
+// Body parser, reading data from body into req.body - max payload size
+
+app.use(express.json({ limit: '10kb' }));
+
+// Data sanatization against NoSql query injection
+
+app.use(mongoSanitize());
+
+// Data sanatization against XSS
+
+app.use(xss());
+
+// Test middleware
 
 app.use((req, res, next) => {
+  console.log('Hi from middleware');
   req.requsetTime = new Date().toISOString();
   next();
 });
