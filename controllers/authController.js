23f3917a6f85819a4c3a19bf509e68abf31fb737
jk21-HH <jeanya12@gmail.com>
2023-09-20 14:13:29 +0300

    implement sending cookie

--- controllers/authController.js
+++ controllers/authController.js
@@ -17,6 +17,17 @@ const signToken = (id) => {
 const createAndSendToken = (user, message, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_COOKIE_EXPIRES_IN * 24 * 60 * 60 * 1000
+    ),
+    httpOnly: true,
+  };
+
+  res.cookie('jwt', token, cookieOptions);
+
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     message,
